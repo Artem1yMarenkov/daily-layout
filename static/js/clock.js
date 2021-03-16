07cd103fc4afa8cd3a67cd8207bb1c9b57a0f846
Artem Marenkov <68229260+Artem1yMarenkov@users.noreply.github.com>
2021-03-16 20:23:02 +0300

    Update clock.js

--- static/js/clock.js
+++ static/js/clock.js
@@ -10,9 +10,8 @@ function clock() {
     if (hours < 10) hours = "0" + hours;
     if (minutes < 10) minutes = "0" + minutes;
     if (seconds < 10) seconds = "0" + seconds;
+    
     document.querySelector('.clock').innerHTML = hours + ":" + minutes + ":" + seconds;
-    document.querySelector('.clock').innerHTML = "  " + hours + ":" + minutes + ":" + seconds;
-    setTimeout("clock()", 1000);
 }
 clock();
 setInterval(clock, 1000);
